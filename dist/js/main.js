39c9c578366881c18936c4a11d561119da069f0d
takeshisakuma <saude0518@gmail.com>
2019-10-17 07:43:34 +0900

    css2scss js2ts

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+var menu = document.getElementsByClassName('switch');
+function show() {
+    var hideContent = this.nextElementSibling;
+    hideContent.classList.toggle('hide');
+    var switchCondition = this.children[0];
+    console.log(switchCondition);
+    switchCondition.classList.toggle('contentClosed');
+}
+for (var i = 0; i < menu.length; i++) {
+    menu[i].addEventListener('click', show);
+}
+//# sourceMappingURL=main.js.map
