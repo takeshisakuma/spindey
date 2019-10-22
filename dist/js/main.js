64e3b4afbe70970ef9f381796cf8db25e6e24c21
takeshisakuma <saude0518@gmail.com>
2019-10-22 20:05:43 +0900

    go top for pc

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,6 @@ function show() {
     var hideContent = this.nextElementSibling;
     hideContent.classList.toggle("hide");
     var switchCondition = this.children[0];
-    console.log(switchCondition);
     switchCondition.classList.toggle("contentClosed");
 }
 /*
@@ -26,4 +25,36 @@ var loadedTransiton = function () {
     }
 };
 window.addEventListener("load", loadedTransiton);
+var returnTop = document.getElementById("js-returntop");
+var wheelScrollCancel = function (event) {
+    event.preventDefault();
+};
+var returnTopAnimation = function () {
+    document.addEventListener('wheel', wheelScrollCancel, { passive: false });
+    var currentScrollPosition = 0;
+    currentScrollPosition = window.scrollY || 0;
+    window.scrollTo(0, Math.floor(currentScrollPosition / 1.1));
+    if (currentScrollPosition > 0) {
+        window.requestAnimationFrame(returnTopAnimation);
+    }
+    else {
+        document.removeEventListener('wheel', wheelScrollCancel, { passive: false });
+    }
+};
+returnTop.addEventListener("click", returnTopAnimation);
+//モニターの高さ取得
+var displayHeight = window.parent.screen.height;
+var returntopAppearance = function () {
+    //スクロール量
+    var currentScrollPosition = 0;
+    currentScrollPosition = window.scrollY || 0;
+    //スクロール量が画面の高さより大きいならactive追加
+    if (displayHeight < currentScrollPosition * 5) {
+        returnTop.classList.add("active");
+    }
+    else {
+        returnTop.classList.remove("active");
+    }
+};
+document.addEventListener("scroll", returntopAppearance);
 //# sourceMappingURL=main.js.map
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,7 +4,6 @@ function show() {
   let hideContent = this.nextElementSibling;
   hideContent.classList.toggle("hide");
   let switchCondition = this.children[0];
-  console.log(switchCondition);
   switchCondition.classList.toggle("contentClosed");
 }
 
@@ -30,3 +29,56 @@ const loadedTransiton = () => {
   }
 }
 window.addEventListener("load", loadedTransiton);
+
+
+
+
+
+
+
+
+const returnTop = document.getElementById("js-returntop");
+
+const wheelScrollCancel = (event) => {
+  event.preventDefault();
+}
+
+const returnTopAnimation = () => {
+
+  document.addEventListener('wheel', wheelScrollCancel, { passive: false });
+
+  let currentScrollPosition = 0;
+
+  currentScrollPosition = window.scrollY || 0;
+
+  window.scrollTo(0, Math.floor(currentScrollPosition / 1.1));
+
+  if (currentScrollPosition > 0) {
+    window.requestAnimationFrame(returnTopAnimation);
+  } else {
+
+    document.removeEventListener('wheel', wheelScrollCancel, { passive: false });
+  }
+}
+
+returnTop.addEventListener("click", returnTopAnimation);
+
+
+//モニターの高さ取得
+const displayHeight = window.parent.screen.height;
+
+const returntopAppearance = () => {
+  //スクロール量
+  let currentScrollPosition = 0;
+  currentScrollPosition = window.scrollY || 0;
+
+  //スクロール量が画面の高さより大きいならactive追加
+  if (displayHeight < currentScrollPosition * 5) {
+    returnTop.classList.add("active");
+  } else {
+    returnTop.classList.remove("active");
+  }
+
+}
+
+document.addEventListener("scroll", returntopAppearance);
